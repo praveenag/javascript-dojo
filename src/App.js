f91a4c15bdefe8064742f6f357b4b77e3ed4d5d1
Rik Jurriaans <rik@jurriaans.org>
2019-01-03 16:01:34 +0000

    fix algo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import check from './check.js'
 
+/* 
+ * [ // every 4th
+ *   1, 0, 0,
+ *   0, 1, 0,
+ *   0, 0, 1
+ * ]
+ * 
+ * [ // every 2th
+ *   0, 0, 1,
+ *   0, 1, 0,
+ *   1, 0, 0
+ * ]
+ * 
+ * [ // every
+ *   1, 1, 1,
+ *   0, 0, 0,
+ *   0, 0, 0
+ * ]
+ *  */
 class App extends Component {
   render() {
+
+    console.log(check(
+      [ // every 3th
+        0, 0, 1,
+        0, 1, 0,
+        1, 0, 0
+      ]
+    ))
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-HELLO WORLD          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
       </div>
     );
   }
---
+++ src/check.js
@@ -0,0 +1,22 @@
+export default (board) => {
+  var n = 3;
+  var result = false;
+
+  for (var i = 0; i < board.length; i++) {
+    result = !!(i == 0 && board[i] === 1 && board[i+n] === 1 && board[i+n+n]) || // Left diagonal
+             !!(i == 2 && board[i] === 1 && board[i+(n-1)] === 1 && board[i+(n-1)+(n-1)]) || // Right diagonal
+             !!(board[i] === 1 && board[i+(n+1)] === 1 && board[i+(n+1)+(n+1)]) || // Vertically Linear
+             !!(board[i] === 1 && board[i+1] === 1 && board[i+1+1]); // Horizontally Linear
+
+    if (result) break;
+  }
+
+  return result;
+
+  /* board.reduce((acc, curr) => {
+   *   console.log(acc, curr)
+   *   if (curr === 1) {
+   *     return acc + 1
+   *   }
+   * }, 0) */
+}
